066dbbb1213c907c8d458fc0446ab9966c874c01
urbconceptac <urbconceptac@gmail.com>
2026-08-21 16:01:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,3 @@
-import { defineConfig } from '@lovable.dev/vite-tanstack-config';
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  base: './',
-  tanstackStart: {
-    spa: true, // Força a compilação em modo SPA estático (sem SSR)
-  },
-});
+export default defineConfig({});
